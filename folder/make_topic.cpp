3526a97073f16fad80c1fcdfc2d4f6844f8ed399
tekitouk <47982907+koduma@users.noreply.github.com>
2023-11-12 00:34:27 +0900

    Update make_topic.cpp

--- folder/make_topic.cpp
+++ folder/make_topic.cpp
@@ -81,17 +81,18 @@ int distance(string x,string y){
       }
     }
   }
-	
 ofstream fi("y.txt");
 fi<<y;
 fi.close();
+    
 system("python3 mecab.py");
+    
 int d2;
 FILE *file;
 file = fopen("score.txt", "r");
 fscanf(file, "%d", &d2);
 fclose(file);
-cout<<d2<<endl;
+    
 return LP[(int)x.size()][(int)y.size()]-(d2*100);
 
 }
@@ -107,7 +108,6 @@ int main(){
 	}
 	if(escape){break;}
 	}
-	
 	ofstream fi("x.txt");
 	fi<<start;
 	fi.close();
@@ -115,11 +115,14 @@ int main(){
     
 	int mind=1000000;
 	int find=-1;
+	int dir=-1;
     
-	for(int i=0;i<=2525;i++){
+	for(int j=1;j<=12;j++){
+	for(int i=((j-1)*4000);i<=(j*4000);i++){    
 	string s=to_string(i);
-	ifstream myfile (s+"_q.txt");    
-	if( !myfile ) {continue;}
+	ifstream myfile ("dir"+to_string(j)+"/"+s+"_q.txt");
+	ifstream myfile2 ("dir"+to_string(j)+"/"+s+"_a.txt");     
+	if( (!myfile) || (!myfile2)) {continue;}
 	string line;
 	string t_path="";
 	while(getline(myfile,line)){
@@ -129,17 +132,20 @@ int main(){
 	while(1){
 	if((int)t_path.size()<CODE_LENGTH){t_path+="@";}
 	else{break;}
-	}   
+	}
+	if((int)start.size()>CODE_LENGTH){start=start.substr(0,CODE_LENGTH);}
+	if((int)t_path.size()>CODE_LENGTH){t_path=t_path.substr(0,CODE_LENGTH);}    
 	int d=distance(start,t_path);
 	if((int)t_path.size()>CODE_LENGTH){d=1000000;}    
-	if(mind>d){mind=d;find=i;}
-	cout<<"i="<<i<<",d="<<d<<endl;
+	if(mind>d){mind=d;find=i;dir=j;}
+	cout<<"dir="<<dir<<",i="<<i<<",d="<<d<<",mind="<<mind<<",find="<<find<<endl;
+	}
 	}
 	
 	string line;
 	string tmp="";
 	string t_path="";
-	ifstream myfile (to_string(find)+"_a.txt");
+	ifstream myfile ("dir"+to_string(dir)+"/"+to_string(find)+"_a.txt");
 	while(getline(myfile,line)){
 	if(line!=""){    
 	t_path+=line+'@';
@@ -149,10 +155,10 @@ int main(){
 	myfile.close();
 	ans=t_path;
 	cout<<find<<"_a.txt"<<endl;
-	for(int i=0;i<(int)ans.size();i++){
+    for(int i=0;i<(int)ans.size();i++){
 	if(ans[i]=='@'){ans[i]='\n';}
 	}
-	cout<<ans<<endl;
+    cout<<ans<<endl;
     
 	return 0;
 }
