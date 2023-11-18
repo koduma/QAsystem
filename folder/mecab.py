dc0d7f5f389973d9b2c3590f23c1a614da3be904
tekitouk <47982907+koduma@users.noreply.github.com>
2023-11-18 22:55:56 +0900

    Update mecab.py

--- folder/mecab.py
+++ folder/mecab.py
@@ -21,14 +21,15 @@ text=str(class_names)
 text2=str(class_names2)
 
 node = mecabTagger.parseToNode(text)
+
 while node:
     word = node.surface
     hinshi = node.feature.split(",")[0]
     ippan = node.feature.split(",")[1]
-    if word in noun_count.keys() and hinshi == "名詞" and ippan == "一般":
+    if word in noun_count.keys() and hinshi == "名詞":#and ippan == "一般":
         noun_freq = noun_count[word]
         noun_count[word] = noun_freq + 1
-    elif hinshi == "名詞" and ippan == "一般":
+    elif hinshi == "名詞":#and ippan == "一般":
         noun_count[word] = 1
     else:
         pass
@@ -41,10 +42,10 @@ while node2:
     word = node2.surface
     hinshi = node2.feature.split(",")[0]
     ippan = node2.feature.split(",")[1]
-    if word in noun_count2.keys() and hinshi == "名詞" and ippan == "一般":
+    if word in noun_count2.keys() and hinshi == "名詞":# and ippan == "一般":
         noun_freq = noun_count2[word]
         noun_count2[word] = noun_freq + 1
-    elif hinshi == "名詞" and ippan == "一般":
+    elif hinshi == "名詞":# and ippan == "一般":
         noun_count2[word] = 1
     else:
         pass
@@ -57,9 +58,8 @@ score=0
 for i in range(len(noun_count)):
     for j in range(len(noun_count2)):
         if noun_count[i][0] == noun_count2[j][0]:
-            print(str(noun_count[i][0])+",score="+str(score+text.count(str(noun_count[i][0]))))
-            #print(noun_count[i][1])
             score=score+text.count(str(noun_count[i][0]))
+            #print(noun_count[i][0]+",score="+str(text.count(str(noun_count[i][0]))))
             break
             
 f = open('score.txt', 'w')
